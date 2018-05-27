b47492d0afe6903d0bb0602ee78f1457be31140b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-import {of as streamOf} from "most"
-import type from "@unction/type"
-
-export default function of (key: KeyType | void): UnaryFunctionType {
-  return function ofKey (value: ValueType): UnaryFunctionType {
-    return function ofKeyValue (functor: FunctorType): UnaryFunctionType {
+import { of as streamOf } from "most";
+import type from "@unction/type";
+export default function of(key) {
+  return function ofKey(value) {
+    return function ofKeyValue(functor) {
       switch (type(functor)) {
-        case "Array": {
-          return [
-            value,
-          ]
-        }
-        case "Object": {
-          return {[key]: value}
-        }
-        case "Set": {
-          return new Set([
-            value,
-          ])
-        }
-        case "Map": {
-          return new Map([
-            [
-              key,
-              value,
-            ],
-          ])
-        }
-        case "String": {
-          return `${value}`
-        }
-        case "Stream": {
-          return streamOf(value)
-        }
-        default: {
-          throw new Error(`of doesn't know how to type ${type(functor)}`)
-        }
+        case "Array":
+          {
+            return [value];
+          }
+
+        case "Object":
+          {
+            return {
+              [key]: value
+            };
+          }
+
+        case "Set":
+          {
+            return new Set([value]);
+          }
+
+        case "Map":
+          {
+            return new Map([[key, value]]);
+          }
+
+        case "String":
+          {
+            return `${value}`;
+          }
+
+        case "Stream":
+          {
+            return streamOf(value);
+          }
+
+        default:
+          {
+            throw new Error(`of doesn't know how to type ${type(functor)}`);
+          }
       }
-    }
-  }
+    };
+  };
 }
