fa83bfc2dcbf04d0b1bbf89d894d1f0e1befddf0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    More flowtype removed

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
+/* eslint-disable flowtype/require-return-type */
 import {test} from "tap"
 import {empty} from "most"
 import streamSatisfies from "@unction/streamsatisfies"
